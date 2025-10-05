07cf806707b2b37ce5223d5af3285d2ed2e2846b
estiven-lg <estivenlaferre@gmail.com>
2025-10-05 13:58:37 -0600

    Proyecto inicial Laravel API

---
+++ app/Http/Controllers/Api/ClienteController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Cliente::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $cliente = Cliente::create($request->all());
+        return response()->json($cliente, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Cliente::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        $cliente->update($request->all());
+        return response()->json($cliente, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Cliente::destroy($id);
+        return response()->json(null, 204);
+    }
+}
